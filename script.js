4a1ba46f27c37449977ebb55459dc0f42c93492a
Tung HOANG <tung.hoang@gmail.com>
2020-09-11 22:04:23 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function clickButton() {
   console.log('Click button')
-  fetch('http://localhost:8000/classes/', {
+  //fetch('http://localhost:8000/classes/', {
+  fetch('http://google.com', {
     method: 'GET'
   }).then(response => response.json())
   .then(data => {
